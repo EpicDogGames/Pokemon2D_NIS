75c2cb5c8b9003de9d6ddd8718a595384ddd304a
OceanGopher <epicdoggames@gmail.com>
2021-10-27 18:09:42 -0500

    Episode 69 Expanded the quest system to include quests that can be marked as completed after speaking to an NPC. This will come into play with StoryItems blocking areas until a quest has been completed. Also made a slight modification to the quest system that if the player already has collected a required item, he can proceed with getting acknowledged for having the item and completing the quest at that point.

--- Assets/Scripts/Character/NPCController.cs
+++ Assets/Scripts/Character/NPCController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class NPCController : MonoBehaviour, Interactable
 {
     [SerializeField] Dialogue dialogue;
+    [Header("Quests")]
     [SerializeField] QuestBase questToStart;
+    [SerializeField] QuestBase questToComplete;
+    [Header("Movement")]
     [SerializeField] List<Vector2> movementPattern;
     [SerializeField] float timeBetweenPattern;
 
@@ -16,11 +19,13 @@ public class NPCController : MonoBehaviour, Interactable
 
     Character character;
     ItemGiver itemGiver;
+    PokemonGiver pokemonGiver;
 
     private void Awake() 
     {
         character = GetComponent<Character>(); 
         itemGiver = GetComponent<ItemGiver>();  
+        pokemonGiver = GetComponent<PokemonGiver>();
     }
 
     private void Update() 
@@ -46,15 +51,33 @@ public class NPCController : MonoBehaviour, Interactable
             state = NPCState.Dialog;
             character.LookTowards(initiator.position);
 
+            if (questToComplete != null)
+            {
+                var quest = new Quest(questToComplete);
+                yield return quest.CompletedQuest(initiator);
+                questToComplete = null;
+                Debug.Log($"{quest.Base.Name} completed");
+            }
+            
             if (itemGiver != null && itemGiver.CanBeGiven())
             {
                 yield return itemGiver.GiveItem(initiator.GetComponent<PlayerController>());
+            }
+            else if (pokemonGiver != null && pokemonGiver.CanBeGiven())
+            {
+                yield return pokemonGiver.GivePokemon(initiator.GetComponent<PlayerController>());
             }
             else if (questToStart != null)
             {
                 activeQuest = new Quest(questToStart);
                 yield return activeQuest.StartQuest();
                 questToStart = null;
+
+                if (activeQuest.CanBeCompleted())
+                {
+                    yield return activeQuest.CompletedQuest(initiator);
+                    activeQuest = null;
+                }
             }
             else if (activeQuest != null)
             {
---
+++ Assets/Scripts/Pokemons/PokemonGiver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonGiver : MonoBehaviour, ISavable
+{
+    [SerializeField] Pokemon pokemonToGive;
+    [SerializeField] Dialogue dialogue;
+    string dialogueText;
+
+    bool used = false;
+
+    public IEnumerator GivePokemon(PlayerController player)
+    {
+        yield return DialogueManager.Instance.ShowDialogue(dialogue);
+
+        pokemonToGive.Init();
+        player.GetComponent<PokemonParty>().AddPokemon(pokemonToGive);
+
+        used = true;
+
+        dialogueText = $"{player.Name} received {pokemonToGive.Base.Name}";
+
+        yield return DialogueManager.Instance.ShowDialogueText(dialogueText);
+    }
+
+    public bool CanBeGiven()
+    {
+        return pokemonToGive != null && !used;
+    }
+
+    public object CaptureState()
+    {
+        return used;
+    }
+
+    public void RestoreState(object state)
+    {
+        used = (bool)state;
+    }
+}
