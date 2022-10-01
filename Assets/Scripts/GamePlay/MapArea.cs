39101fc879e008a591156522239b070bed2c2168
OceanGopher <epicdoggames@gmail.com>
2022-10-01 18:48:17 -0500

    Episodes 89-90 Improving the wild encounters so that a chance percentage of wild pokemon are available in an encounter. Select a random pokemon based on its chance value with the values all adding up to 100%. Can have multiple levels for encounters with multiple pokemons available for the encounter with the percentage determining which is more likely for an encounter. Created a custom editor in Unity to make sure in the MapArea, the total chance percentage of the wild pokemon will have to equal 100 percentage or an error message is displayed in the editor.

--- Assets/Scripts/GamePlay/MapArea.cs
+++ Assets/Scripts/GamePlay/MapArea.cs
@@ -1,15 +1,55 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapArea : MonoBehaviour
 {
-    [SerializeField] List<Pokemon> wildPokemons;
+    [SerializeField] List<PokemonEncounterRecord> wildPokemons;
+
+    [HideInInspector]
+    [SerializeField] int totalChance = 0;
+
+    private void OnValidate() 
+    {
+        // calculate upper and lower chance
+        // sum of chances of the pokemon must equal 100 .. this is done in the inspector
+        totalChance = 0;
+        foreach (var record in wildPokemons)
+        {
+            record.chanceLower = totalChance;
+            record.chanceUpper = totalChance + record.chancePercentage;
+
+            totalChance = totalChance + record.chancePercentage;
+        }
+    }
+
+    private void Start() 
+    {
+       
+    }
 
     public Pokemon GetRandomWildPokemon()
     {
-        var wildPokemon = wildPokemons[Random.Range(0, wildPokemons.Count)];
+        int randomValue = Random.Range(1, 101);
+        var pokemonRecord = wildPokemons.First(p => randomValue >= p.chanceLower && randomValue <= p.chanceUpper);
+
+        var levelRange = pokemonRecord.levelRange;
+        int level = levelRange.y == 0 ? levelRange.x : Random.Range(levelRange.x, levelRange.y+1);
+
+        var wildPokemon = new Pokemon(pokemonRecord.pokemon, level);
         wildPokemon.Init();
         return wildPokemon;
     }
+}
+
+[System.Serializable]
+public class PokemonEncounterRecord
+{
+    public PokemonBase pokemon;
+    public Vector2Int levelRange;           // use Vector2Int because you can enter a range of values
+    public int chancePercentage;
+
+    public int chanceLower { get; set; }
+    public int chanceUpper { get; set; }
 }
